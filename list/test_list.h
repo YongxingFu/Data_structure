91ae3aa6f4a3425a2769e828d63fcb8a2f593706
FYX <1024118000@qq.com>
2021-03-20 22:05:29 +0800

    test1

--- list/test_list.h
+++ list/test_list.h
@@ -1,4 +1,12 @@
 #ifndef _TEST_LIST_H
 #define _TEST_LIST_H
 
+typedef unsigned int ElementType;
+typedef unsigned int* Position;
+
+typedef struct _node
+{
+    ElementType ElementType;
+    Position position;
+}ListNode_st;
 #endif // !_TEST_LIST_H1
