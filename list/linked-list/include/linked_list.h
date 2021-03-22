a10a078e87243968d3ba401c679430f0073dc4ed
FYX <1024118000@qq.com>
2021-03-23 00:05:57 +0800

    nn

---
+++ list/linked-list/include/linked_list.h
@@ -0,0 +1,15 @@
+#ifndef _LINKED_LIST_H_
+#define _LINKED_LIST_H_
+
+typedef struct _NodeData
+{
+
+}
+
+typedef struct _Node
+{
+
+    struct _node* p_nextnode;
+}Node;
+
+#endif // _LINKED_LIST_H_
